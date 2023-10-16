f451a38421db8fe8a148f4f99082fd7ad2db4a2f
Horia Dobrin <horia.dobrin@gmail.com>
2023-10-16 14:57:37 +0300

    Adeed Bcrypt for a more secure Password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,17 +3,34 @@ import { User } from './user.model';
 import { UserDto } from './user-dtos/user-credentials.dto';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './user-dtos/jwt-payload.interface';
-import { readDataFromFile } from 'src/data/readWriteData';
+import { readDataFromFile, writeDataToFile } from 'src/data/readWriteData';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  private admin: User = {
+  private static admin: User = {
     id: '1',
     email: 'user@email.com',
     password: 'pass',
   };
 
-  constructor(private jwtService: JwtService) {}
+  constructor(private jwtService: JwtService) {
+    //Thought to delete the file before generating a user with a hashed + salted password
+    writeDataToFile('users', []);
+    UserService.createAdmin();
+  }
+
+  //Purpose of creating a more secure password and storing it in the file
+  private static async createAdmin() {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(this.admin.password, salt);
+
+    this.admin.password = hashedPassword;
+    const data: User[] = await readDataFromFile('users');
+    data.push(this.admin);
+
+    writeDataToFile('users', data);
+  }
 
   async signIn(user: UserDto): Promise<{ accessToken: string }> {
     const { email, password } = user;
@@ -21,8 +38,10 @@ export class UserService {
     const usersFromFile = await readDataFromFile<User>('users');
 
     const theAdmin = usersFromFile[0];
-
-    if (email === theAdmin.email && password === theAdmin.password) {
+    if (
+      email === theAdmin.email &&
+      (await bcrypt.compare(password, theAdmin.password))
+    ) {
       const payload: JwtPayload = { email };
 
       const accessToken = this.jwtService.sign(payload);
